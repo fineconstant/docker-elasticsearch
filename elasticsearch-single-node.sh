f8f0101c620bed06ae36f48cf15f784512a1700e
Kamil Duda <kamilduda01@gmail.com>
2017-04-15 14:52:19 +0200

    Initial commit

---
+++ elasticsearch-single-node.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+IMAGE=docker.elastic.co/elasticsearch/elasticsearch
+VERSION=5.3.0
+
+docker run -d -p 9200:9200 --name docker-elasticsearch \
+ -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" \
+ -e "cluster.name=elasticsearch-docker" -e "node.name=elasticsearch-docker" \
+ -e "bootstrap.memory_lock=true" \
+ ${IMAGE}:${VERSION}
